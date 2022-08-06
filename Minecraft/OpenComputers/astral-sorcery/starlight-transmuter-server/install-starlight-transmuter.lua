8dfeb95538c1c399c78731e59caffdcae009ca60
JasonQ1232 <47706293+JasonQ1232@users.noreply.github.com>
2022-08-06 17:07:52 +0100

    Update install-starlight-transmuter.lua

--- Minecraft/OpenComputers/astral-sorcery/starlight-transmuter-server/install-starlight-transmuter.lua
+++ Minecraft/OpenComputers/astral-sorcery/starlight-transmuter-server/install-starlight-transmuter.lua
@@ -1,3 +1,5 @@
 local shell = require "shell"
 
-shell.execute("mkdir /home/testdir")
+shell.execute("wget https://github.com/JasonQ1232/Game-Scripts/blob/15aef93c96e3ede609ab7e2fb609c12108d84894/Minecraft/OpenComputers/astral-sorcery/starlight-transmuter-server/starlight-transmuter.lua /etc/rc.d/starlight-transmuter.lua")
+shell.execute("mkdir /etc/rc.d/starlight-transmuter")
+shell.execute("wget https://github.com/JasonQ1232/Game-Scripts/blob/15aef93c96e3ede609ab7e2fb609c12108d84894/Minecraft/OpenComputers/astral-sorcery/starlight-transmuter-server/script.lua /etc/rc.d/starlight-transmuter/script.lua")
