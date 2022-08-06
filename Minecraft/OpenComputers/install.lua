a23e521c8f0debd211d30dbcc98409fa1ef4631d
JasonQ1232 <47706293+JasonQ1232@users.noreply.github.com>
2022-08-06 23:23:05 +0100

    Create install.lua

---
+++ Minecraft/OpenComputers/install.lua
@@ -0,0 +1,10 @@
+local shell = require "shell"
+
+
+shell.execute("wget -f https://raw.githubusercontent.com/JasonQ1232/Game-Scripts/main/Minecraft/OpenComputers/blood-magic/slates-v3.lua /home/slates-v3.lua")
+
+shell.execute("mkdir /home/installers")
+shell.execute("wget -f https://raw.githubusercontent.com/JasonQ1232/Game-Scripts/main/Minecraft/OpenComputers/greg-tech/diesel-engine-toggle/install-diesel-engine-toggle.lua /home/install-diesel-engine-toggle.lua")
+shell.execute("/home/install-diesel-engine-toggle.lua")
+shell.execute("wget -f https://raw.githubusercontent.com/JasonQ1232/Game-Scripts/main/Minecraft/OpenComputers/astral-sorcery/starlight-transmuter-server/install-starlight-transmuter.lua /home/install-starlight-transmuter.lua")
+shell.execute("/home/install-starlight-transmuter.lua")
