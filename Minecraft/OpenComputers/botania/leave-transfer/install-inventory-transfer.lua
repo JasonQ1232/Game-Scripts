3da76b64da75fdfebefe96634e31b0ba0ba33190
JasonQ1232 <47706293+JasonQ1232@users.noreply.github.com>
2022-08-06 21:28:40 +0100

    Update install-inventory-transfer.lua

--- Minecraft/OpenComputers/botania/leave-transfer/install-inventory-transfer.lua
+++ Minecraft/OpenComputers/botania/leave-transfer/install-inventory-transfer.lua
@@ -5,8 +5,8 @@ local rc = require "rc"
 
 
 shell.execute("mkdir /etc/rc.d/leave-transfer")
-shell.execute("wget -f https://raw.githubusercontent.com/JasonQ1232/Game-Scripts/main/Minecraft/OpenComputers/astral-sorcery/leave-transfer-server/leave-transfer.lua /etc/rc.d/leave-transfer/leave-transfer.lua")
-shell.execute("wget -f https://raw.githubusercontent.com/JasonQ1232/Game-Scripts/main/Minecraft/OpenComputers/astral-sorcery/leave-transfer-server/script.lua /etc/rc.d/leave-transfer/script.lua")
+shell.execute("wget -f https://raw.githubusercontent.com/JasonQ1232/Game-Scripts/main/Minecraft/OpenComputers/botania/leave-transfer/script.lua /etc/rc.d/leave-transfer/leave-transfer.lua")
+shell.execute("wget -f https://raw.githubusercontent.com/JasonQ1232/Game-Scripts/main/Minecraft/OpenComputers/botania/leave-transfer/script.lua /etc/rc.d/leave-transfer/script.lua")
 rc.unload("leave-transfer/leave-transfer")
 shell.execute("rc leave-transfer/leave-transfer enable")
 shell.execute("rc leave-transfer/leave-transfer restart")
