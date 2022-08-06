e2ae75d9cb19ce85445b856e0ae848580b0e6883
JasonQ1232 <47706293+JasonQ1232@users.noreply.github.com>
2022-08-06 18:08:49 +0100

    s

--- Minecraft/OpenComputers/astral-sorcery/starlight-transmuter-server/install-starlight-transmuter.lua
+++ Minecraft/OpenComputers/astral-sorcery/starlight-transmuter-server/install-starlight-transmuter.lua
@@ -1,5 +1,8 @@
 local shell = require "shell"
+local term = require("term")
 
-shell.execute("wget https://github.com/JasonQ1232/Game-Scripts/blob/15aef93c96e3ede609ab7e2fb609c12108d84894/Minecraft/OpenComputers/astral-sorcery/starlight-transmuter-server/starlight-transmuter.lua /etc/rc.d/starlight-transmuter.lua")
+shell.execute("wget -f https://raw.githubusercontent.com/JasonQ1232/Game-Scripts/15aef93c96e3ede609ab7e2fb609c12108d84894/Minecraft/OpenComputers/astral-sorcery/starlight-transmuter-server/starlight-transmuter.lua /etc/rc.d/starlight-transmuter.lua")
 shell.execute("mkdir /etc/rc.d/starlight-transmuter")
-shell.execute("wget https://github.com/JasonQ1232/Game-Scripts/blob/15aef93c96e3ede609ab7e2fb609c12108d84894/Minecraft/OpenComputers/astral-sorcery/starlight-transmuter-server/script.lua /etc/rc.d/starlight-transmuter/script.lua")
+shell.execute("wget -f https://raw.githubusercontent.com/JasonQ1232/Game-Scripts/15aef93c96e3ede609ab7e2fb609c12108d84894/Minecraft/OpenComputers/astral-sorcery/starlight-transmuter-server/script.lua /etc/rc.d/starlight-transmuter/script.lua")
+
+term.write("Remember to add 'starlight-transmuter' to the /etc/rc.cfg file.")
--- Minecraft/OpenComputers/astral-sorcery/starlight-transmuter-server/script.lua
+++ Minecraft/OpenComputers/astral-sorcery/starlight-transmuter-server/script.lua
@@ -1,1 +1,47 @@
-t
+local os = require("os")
+local component = require("component") 
+local sides = require("sides")
+local colors = require("colors")
+local geo = component.geolyzer
+
+rs_address = "f78"
+rs_side = sides.top
+geo_side = sides.top
+
+rs = component.proxy(component.get(rs_address))
+
+whitelist = {
+    "gregtech:ore_uranium_0", 
+    "minecraft:clay", 
+    "minecraft:end_stone", 
+    "gregtech:compressed_7", 
+    "contenttweaker:sub_block_holder_0",
+    "contenttweaker:sub_block_holder_1",
+    "contenttweaker:sub_block_holder_3",
+    "contenttweaker:sub_block_holder_4",
+    "contenttweaker:sub_block_holder_5",
+    "contenttweaker:sub_block_holder_6",
+    "actuallyadditions:block_crystal_cluster_redstone",
+    "actuallyadditions:block_crystal_cluster_lapis",
+    "actuallyadditions:block_crystal_cluster_coal",
+    "actuallyadditions:block_crystal_cluster_diamond",
+    "actuallyadditions:block_crystal_cluster_iron"
+}
+
+function scan()
+    while true do
+        result_table = geo.analyze(geo_side)
+        for block in pairs(whitelist) do
+            block_name = whitelist[block]
+            if(result_table.name == block_name) then
+                rs.setOutput(rs_side, 15)
+                os.sleep(0.2)
+                rs.setOutput(rs_side, 0)
+            end
+        end
+    end
+end
+
+while true do
+    scan()
+end
