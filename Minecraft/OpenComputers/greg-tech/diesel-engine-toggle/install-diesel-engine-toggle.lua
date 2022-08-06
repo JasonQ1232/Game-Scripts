e42e07dff131999ffab249906158d19120e05308
JasonQ1232 <47706293+JasonQ1232@users.noreply.github.com>
2022-08-06 23:03:10 +0100

    Update install-diesel-engine-toggle.lua

--- Minecraft/OpenComputers/greg-tech/diesel-engine-toggle/install-diesel-engine-toggle.lua
+++ Minecraft/OpenComputers/greg-tech/diesel-engine-toggle/install-diesel-engine-toggle.lua
@@ -5,8 +5,8 @@ local rc = require "rc"
 
 
 shell.execute("mkdir /etc/rc.d/diesel-engine-toggle")
-shell.execute("wget -f https://raw.githubusercontent.com/JasonQ1232/Game-Scripts/main/Minecraft/OpenComputers/botania/diesel-engine-toggle/script.lua /etc/rc.d/diesel-engine-toggle/diesel-engine-toggle.lua")
-shell.execute("wget -f https://raw.githubusercontent.com/JasonQ1232/Game-Scripts/main/Minecraft/OpenComputers/botania/diesel-engine-toggle/script.lua /etc/rc.d/diesel-engine-toggle/script.lua")
+shell.execute("wget -f https://raw.githubusercontent.com/JasonQ1232/Game-Scripts/main/Minecraft/OpenComputers/greg-tech/diesel-engine-toggle/script.lua /etc/rc.d/diesel-engine-toggle/diesel-engine-toggle.lua")
+shell.execute("wget -f https://raw.githubusercontent.com/JasonQ1232/Game-Scripts/main/Minecraft/OpenComputers/greg-tech/diesel-engine-toggle/script.lua /etc/rc.d/diesel-engine-toggle/script.lua")
 rc.unload("diesel-engine-toggle/diesel-engine-toggle")
 shell.execute("rc diesel-engine-toggle/diesel-engine-toggle enable")
 shell.execute("rc diesel-engine-toggle/diesel-engine-toggle restart")
