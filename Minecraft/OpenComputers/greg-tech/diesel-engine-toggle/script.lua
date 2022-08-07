f87160d14ec0c8daba8ede0b165fff04328eccd0
JasonQ1232 <47706293+JasonQ1232@users.noreply.github.com>
2022-08-07 03:09:58 +0100

    Update script.lua

--- Minecraft/OpenComputers/greg-tech/diesel-engine-toggle/script.lua
+++ Minecraft/OpenComputers/greg-tech/diesel-engine-toggle/script.lua
@@ -21,29 +21,32 @@ while true do
     for injector in pairs(injectors) do
         max_energy = injectors[injector].getMaxEnergyStored()
         cur_energy = injectors[injector].getEnergyStored()
-        -- term.write(injector .. " : " .. cur_energy .. "\n")
+        term.write(injector .. " : " .. cur_energy .. "\n")
         if (cur_energy < (max_energy * 0.20)) then
             table.insert(status, true)
+            term.write("true" .. "\n")
         elseif (cur_energy > (max_energy * 0.75)) then
             table.insert(status, false)
+            term.write("false" .. "\n")
         end
     end
 
     activate = false
-    for i=1, status do
+    for i in pairs(status) do
         if (status[i] == true) then
             activate = true
+            term.write("activate generator" .. "\n")
             break
         end
     end
 
-    if (active) then
+    if (activate) then
         rs.setOutput(rs_side, 15)
     else
         rs.setOutput(rs_side, 0)
     end
 
-    for i=1, status do
+    for i in pairs(status) do
         status[i] = nil
     end
 
